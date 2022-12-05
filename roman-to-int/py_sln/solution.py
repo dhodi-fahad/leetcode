cff18eebb996e7ef2e697da7c26f316c0e2a9cc6
dhodi-fahad <dhodifahad@gmail.com>
2022-12-06 00:19:03 +0300

    update working sln

--- roman-to-int/py_sln/solution.py
+++ roman-to-int/py_sln/solution.py
@@ -2,8 +2,7 @@
 
 
 def romanToInt(s)->str:
-    final_value = 0
-    myHashTable = {
+    value = {
         'I':1,
         'V':5,
         'X':10,
@@ -12,27 +11,62 @@ def romanToInt(s)->str:
         'D':500,
         'M':1000
     }
-    position = 0
-    for i in range(len(s)):
-        if( position<len(s) and s[position] == 'I' and s[position+1] == 'V'):
-            final_value = final_value + 4
-            position = position + 2
-        elif(position<len(s) and s[position] == 'I' and s[position+1] == 'X'):
-            final_value = final_value + 9
-            position = position + 2
-        elif(position<len(s) and s[position] == 'X' and s[position+1] == 'L'):
-            final_value = final_value + 40
-            position = position + 2
-        elif(position<len(s) and s[position] == 'X' and s[position+1] == 'C'):
-            final_value = final_value + 90
-            position = position + 2
-        elif(position<len(s) and s[position] == 'C' and s[position+1] == 'D'):
-            final_value = final_value + 400
-            position = position + 2
-        elif(position<len(s) and s[position] == 'C' and s[position+1] == "M"):
-            final_value = final_value + 900
-            position = position + 2
+    
+    res = 0
+    i = 0
+ 
+    while (i < len( s)):
+ 
+        # Getting value of symbol s[i]
+        s1 = value[s[i]]
+ 
+        if (i + 1 < len( s)):
+ 
+            # Getting value of symbol s[i + 1]
+            s2 = value[s[i + 1]]
+ 
+            # Comparing both values
+            if (s1 >= s2):
+ 
+                # Value of current symbol is greater
+                # or equal to the next symbol
+                res = res + s1
+                i = i + 1
+            else:
+ 
+                # Value of current symbol is greater
+                # or equal to the next symbol
+                res = res + s2 - s1
+                i = i + 2
         else:
-            final_value = final_value + myHashTable[s[position]]
+            res = res + s1
+            i = i + 1
+ 
+    return res
+
+
+    # while position<i:
+    #     if((position+1)<i):
+    #         if(s[position] == 'I' and s[position+1] == 'V'):
+    #             final_value = final_value + 4
+    #             position = position + 2
+    #         elif(s[position] == 'I' and s[position+1] == 'X'):
+    #             final_value = final_value + 9
+    #             position = position + 2
+    #         elif(s[position] == 'X' and s[position+1] == 'L'):
+    #             final_value = final_value + 40
+    #             position = position + 2
+    #         elif(s[position] == 'X' and s[position+1] == 'C'):
+    #             final_value = final_value + 90
+    #             position = position + 2
+    #         elif(s[position] == 'C' and s[position+1] == 'D'):
+    #             final_value = final_value + 400
+    #             position = position + 2
+    #         elif(s[position] == 'C' and s[position+1] == "M"):
+    #             final_value = final_value + 900
+    #             position = position + 2
+    #     else:
+    #         final_value = final_value + myHashTable[s[position]]
+    #         position = position + 1
             
-    return final_value
+    # return final_value
